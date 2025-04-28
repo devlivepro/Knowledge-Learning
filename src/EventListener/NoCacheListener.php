85582182ca165a2063573a6ab447a134a8b9e984
david <david.saadoun@hotmail.com>
2025-04-28 19:11:38 +0200

    Add folder and files

---
+++ src/EventListener/NoCacheListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+class NoCacheListener
+{
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        $request = $event->getRequest();
+        $response = $event->getResponse();
+
+        // Liste des routes sensibles sans cache
+        $routesNoCache = [
+            'app_login',
+            'app_register',
+            'app_forgot_password_request',
+        ];
+
+        if (in_array($request->attributes->get('_route'), $routesNoCache, true)) {
+            $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate');
+            $response->headers->set('Pragma', 'no-cache');
+            $response->headers->set('Expires', '0');
+        }
+    }
+}
