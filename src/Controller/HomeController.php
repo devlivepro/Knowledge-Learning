21a7375e8583dbe4160390d84f661527122e67fb
david <david.saadoun@hotmail.com>
2025-05-04 19:25:11 +0200

    Add

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ThemeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,16 +11,37 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
-    {
+    public function index(
+        AuthenticationUtils $authenticationUtils,
+        ThemeRepository $themeRepo
+    ): Response {
         // Recovers connection error if any
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // Retrieves the last user name entered
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        // Render the view with the variables required for the connection form
+        // Get all themes for the showcase
+        $themes = $themeRepo->findAll();
+
         return $this->render('home/home.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+            'themes'        => $themes,
+        ]);
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Retrieve the last authentication error, if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Retrieve the last username entered by the user (for prefilling the login form)
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // Render the login template, passing in the last username and any login error
+        return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
