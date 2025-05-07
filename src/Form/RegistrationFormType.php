2bbf7cd4c98115302a221ec3ba75b4c2e2da8ba9
david <david.saadoun@hotmail.com>
2025-05-07 19:13:41 +0200

    Updates

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -9,6 +9,9 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
@@ -16,17 +19,38 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('username', TextType::class, [
-                'label' => 'Nom d\'utilisateur',
+                'label'    => 'Nom d\'utilisateur',
                 'required' => true,
             ])
             ->add('email', EmailType::class, [
-                'label' => 'Email'
+                'label'    => 'Email',
+                'required' => true,
             ])
             ->add('plainPassword', PasswordType::class, [
-                'label' => 'Mot de passe',
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-            ]);
+                'label'       => 'Mot de passe',
+                'mapped'      => false,
+                'attr'        => [
+                    'autocomplete' => 'new-password',
+                    'placeholder'  => 'Au moins 8 caractères, 1 majuscule, 1 chiffre'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un mot de passe.',
+                    ]),
+                    new Length([
+                        'min'        => 8,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères.',
+                        // Security: maximum length managed by Symfony
+                        'max'        => 4096,
+                    ]),
+                    new Regex([
+                        // At least one uppercase letter, one lowercase letter and one number
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
+                        'message' => 'Le mot de passe doit contenir au moins une majuscule, une minuscule et un chiffre.',
+                    ]),
+                ],
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
